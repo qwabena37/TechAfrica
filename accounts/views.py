8e246f039cd1c536bcf6c279c6566be5281eab9a
James <qjaymce37@gmail.com>
2025-12-17 11:58:32 +0000

    create accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import ExpertProfile
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class ProfileView(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+
+
+def get_object(self):
+    return self.request.user

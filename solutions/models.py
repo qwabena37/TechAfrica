c3bafddc4a99d02f419dda87c3304530cead5ddf
James <qjaymce37@gmail.com>
2025-12-17 12:06:20 +0000

    modify solutions/models.py for experts only

--- solutions/models.py
+++ solutions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from issues.models import Issue
+from accounts.models import User
 
-# Create your models here.
+
+class Solution(models.Model):
+    issue = models.ForeignKey(Issue, related_name='solutions', on_delete=models.CASCADE)
+    expert = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    image = models.ImageField(upload_to='solutions/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)

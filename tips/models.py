39a9332be78d273bc08d8a14da6afcba17074be2
James <qjaymce37@gmail.com>
2025-12-17 12:21:41 +0000

    modify tips/models.py

--- tips/models.py
+++ tips/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 class TechTip(models.Model):
-    expert = models.ForeignKey(User, on_delete=models.CASCADE)
+    expert = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='tech_tips'
+    )
     title = models.CharField(max_length=255)
     content = models.TextField()
     image = models.ImageField(upload_to='tips/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title

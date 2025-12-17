d14672d8fab21eba173938b6a1cf613b0cbd3eab
James <qjaymce37@gmail.com>
2025-12-17 16:26:46 +0000

    configure JWT in accounts/urls.py to use JWT

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import ProfileView
+from .views import ProfileView, RegisterView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('me/', ProfileView.as_view(), name='my-profile'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
+

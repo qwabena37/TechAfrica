12fd49f5c81123478f405dc56228cacb79e9a09a
James <qjaymce37@gmail.com>
2025-12-17 12:04:24 +0000

    modify issues/views.py

--- issues/views.py
+++ issues/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Issue
+from .serializers import IssueSerializer
 
-# Create your views here.
+
+class IssueListCreateView(generics.ListCreateAPIView):
+    queryset = Issue.objects.all().order_by('-created_at')
+    serializer_class = IssueSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+def perform_create(self, serializer):
+    serializer.save(user=self.request.user)

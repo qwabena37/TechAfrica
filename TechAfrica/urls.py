9961af48ed012b7d8031ee5c994ca499a03c17a1
James <qjaymce37@gmail.com>
2025-12-17 16:56:41 +0000

    add swagger docs

--- TechAfrica/urls.py
+++ TechAfrica/urls.py
@@ -18,6 +18,23 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TechAfrica API",
+        default_version='v1',
+        description="TechAfrica Backend API Documentation",
+        contact=openapi.Contact(email="support@techafrica.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),

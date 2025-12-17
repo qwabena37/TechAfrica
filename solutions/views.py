d7aad6b1fce86be9c883f80c90f666929ceb371b
James <qjaymce37@gmail.com>
2025-12-17 12:10:00 +0000

    modify solutions/views.py

--- solutions/views.py
+++ solutions/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Solution
+from .serializers import SolutionSerializer
+from .permissions import IsExpert
 
-# Create your views here.
+
+class SolutionCreateView(generics.CreateAPIView):
+    serializer_class = SolutionSerializer
+    permission_classes = [IsExpert]
+
+
+def perform_create(self, serializer):
+    serializer.save(expert=self.request.user)

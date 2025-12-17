d9c7a825baf88265e199216dbf126992b1a5ad01
James <qjaymce37@gmail.com>
2025-12-17 21:17:01 +0000

    modify TechAfrica urls.py

--- TechAfrica/urls.py
+++ TechAfrica/urls.py
@@ -1,21 +1,5 @@
-"""
-URL configuration for TechAfrica project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import permissions
@@ -35,32 +19,40 @@ schema_view = get_schema_view(
     permission_classes=[permissions.AllowAny],
 )
 
-
-
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+
+    # API routes
     path('api/accounts/', include('accounts.urls')),
     path('api/issues/', include('issues.urls')),
     path('api/solutions/', include('solutions.urls')),
-      # Swagger as homepage
+
+    # Swagger UI as homepage
     path(
         '',
         schema_view.with_ui('swagger', cache_timeout=0),
         name='swagger-home',
     ),
 
-    # Swagger formats
-    path(
-        'swagger/',
-        schema_view.with_ui('swagger', cache_timeout=0),
-        name='schema-swagger-ui',
+    # OpenAPI JSON/YAML (REQUIRED)
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json',
     ),
+
+    # ReDoc
     path(
         'redoc/',
         schema_view.with_ui('redoc', cache_timeout=0),
         name='schema-redoc',
     ),
-
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Media files (image uploads)
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )

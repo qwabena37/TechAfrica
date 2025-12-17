a7028fc733647527b001fe888f5a1aa960b5a00e
James <qjaymce37@gmail.com>
2025-12-17 12:00:21 +0000

    modify issues/models.py

--- issues/models.py
+++ issues/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Issue(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='issues/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)

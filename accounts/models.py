8b5e144e05e171bb6df92db9cc6353e9399c4917
James <qjaymce37@gmail.com>
2025-12-17 11:53:44 +0000

    modify accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('user', 'Normal User'),
+        ('expert', 'Tech Expert'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
+    country = models.CharField(max_length=50, default='Ghana')
+
+
+@property
+def is_expert(self):
+    return self.role == 'expert'

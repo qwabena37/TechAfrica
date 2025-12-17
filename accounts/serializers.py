efee9ac54bd5e8d370db5d5b8ced49e730cfefce
James <qjaymce37@gmail.com>
2025-12-17 16:21:39 +0000

    configure JWT in accounts/serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,10 +6,27 @@ class UserSerializer(serializers.ModelSerializer):
     expert_badge = serializers.SerializerMethodField()
 
 
-class Meta:
-    model = User
-    fields = ['id', 'username', 'email', 'role', 'expert_badge']
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'role', 'expert_badge']
 
 
-def get_expert_badge(self, obj):
-    return '✔ Expert' if obj.is_expert else None
+    def get_expert_badge(self, obj):
+        return '✔ Expert' if obj.is_expert else None
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'role']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            role=validated_data.get('role', 'user')
+        )
+        return user
